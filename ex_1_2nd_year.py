48e51de5620d6ec10b7fd1ab8911fa230356907e
Tony Hajdini <hajdinim@uni.coventry.ac.uk>
2017-10-03 15:22:09 +0100

    delete substring!

--- ex_1_2nd_year.py
+++ ex_1_2nd_year.py
@@ -3,7 +3,20 @@ import sys
 
 
 
-def del_substring(string):
-    string.split()
-    for i in string:
-        print(string)
+
+
+
+def del_substring():
+    person = input('Enter your string: ')
+    hey = list(person)
+    print ("This is how many charachters you gave me :" + str(len(hey)))
+    for i in hey:
+        print(i)
+    deleted = input('Enter the substring you want to delete: ')
+    hey.remove(deleted)
+    for i in hey: 
+        print(i)
+
+#del_substring()
+
+
