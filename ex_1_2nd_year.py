b45507d172511ba325c96581e79b5fa748cb0495
Tony_Hajdini <hajdinim@uni.coventry.ac.uk>
2017-10-10 15:06:59 +0300

    solve_equation

--- ex_1_2nd_year.py
+++ ex_1_2nd_year.py
@@ -1,17 +1,13 @@
-
 import re
-
-
-
+from sympy.solvers import solve
+from sympy import Symbol
+import sys
+import numpy as np
 
 """
 1. Write a function that deletes a substring from a given character string, specifying the beginning
 position and the length of the substring.
-
-
 """
-
-
 def del_substring():
     person = input('Enter your string: ')
     hey = list(person)
@@ -23,8 +19,6 @@ def del_substring():
     for i in hey: 
         print(i)
 #del_substring()
-
-
 
 """
 2. Read from the keyboard a list of positive integers, for example: 1, 4, 7, 9
@@ -42,15 +36,11 @@ def positive():
             int(re.search(r'\d+', n).group())
         elif eval("n") == 0:
             print ("Oops!")
-            
 def binary(number_list):
     for n in number_list:
         n =  int(re.search(r'\d+', bin(n)[2:] ).group())
         print (n)
-
 # binary(number_list)
-
-
 def binary1():
 
     number_list = [1,4,7,9]
@@ -63,21 +53,7 @@ def binary1():
             for i in number_list:
                 s = s+1
                 print (i,s)
-
-            # print("{} This is a palindrome".format(ns))
 # binary1()
-
-def binary_2():
-    number_list = [1, 4, 7, 9]    
-    """ takes a list an returns the Palindrome, and remove them afterwards """
-    for n in number_list:
-        # if bin(n).replace("0b","") ==  
-        print(n)
-# binary_2()
-
-
-    
-
 number_list = [45, 4, 7, 9]
 def kaprekar_num(number_list):
     """ Takes numbers and return the Kaprekar ones."""
@@ -88,16 +64,31 @@ def kaprekar_num(number_list):
             f = int(firstpart) + int(secondpart) # adding the two parts.    
             if f == i:
                 return i
-kaprekar_num(number_list)
-from sympy.solvers import solve
-from sympy import Symbol 
+#kaprekar_num(number_list)
+"""
+2x + y + 3z = 14 
+x - y + z = 4
+x + 3y - z = 2 
+Output: x = 2, y = 1, z = 3
+"""
 def equation():
-    x,y,z = Symbol("x y z")
-    s = (2*x+y+3*z=14,x,y,z)
-    solve(s)
-    print (k)
-    
-    
-
+    a = np.array([[2,1,3],
+                  [1,-1,1],
+                  [1,3,-1],])
+    b = np.array([[14],[4],[2]])
+    z = np.linalg.solve(a,b)
+    s = z.tolist()
+    list_ = ["x =" , "y = " , "z = "]
+    num = -1
+    for i in s:
+        num = num + 1
+        print(list_[num],i)
 equation()
+
+
+
+
+
+
+
 
