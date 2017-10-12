c1f248787ca1c310adf6e7a3bc38f50fac66933d
Tony Hajdini <hajdinim@uni.coventry.ac.uk>
2017-10-12 06:20:27 +0100

    Armstrong num | Fixed!

--- week_1.py
+++ week_1.py
@@ -8,19 +8,24 @@ def combine_str(string_no1, string_no2):
             str_list.append(string_no2[i])
     return print("".join(str_list)) # makes the list(str_list) a string 
 
-combine_str("1dfghj34","5xjlkjlkj8")
+#combine_str("1dfghj34","5xjlkjlkj8")
 
 
 
 
 def armstrong_num(number):
     """ True if a given number is armstrong """
-    ip = 0
-    for i in map(int,str(number)):
-        ip = ip + i**len(str(number))
-        if number == ip:
-            print("{},Is an armstrong".format(number))
-armstrong_num(1634)
+    if number >= 2:
+        ip,counter = 0,0
+        for i in map(int,str(number)):
+            counter = counter + 1            
+            ip = ip + i**len(str(number))
+            if number == ip:
+                return print("{}, Is an armstrong".format(number))
+            elif len(str(number)) == counter:
+                print("{}, It is not armstrong".format(number))
+armstrong_num(372)
 def test_armstrong():
     for number in range(1000):
         return armstrong_num(number)
+# test_armstrong()
