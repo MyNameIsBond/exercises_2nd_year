e88000c5ea342be0121d6993340d630549c316e9
Tony Hajdini <MyNameIsBond@users.noreply.github.com>
2017-10-18 01:08:28 +0100

    Update week_1.py

--- week_1.py
+++ week_1.py
@@ -68,8 +68,18 @@ Gold          4  kg      100
  
 Output:Load composition value = 790      4 kg of gold and 6 kg of copper 
 """
-def kg_carry(price):
-    sdsd
-
-    setsd
+def ks_larry():
+  material = {'gold':5,'Copper':9,'Plastic':15}
+  carried_kg = int(input('How many kg you want to be crried out.'))
+  print('mater\t Qua\tkg')
+  print ('--------------------')
+  for m,q in material.items():
+    carried_kg = carried_kg - q
+    # if carried_kg >= 0:
+      # carried_kg = max(0,carried_kg)
+    q = q - carried_kg
+    print('{}\t:{}\t{}'.format(m,q,max(0,carried_kg)))
+  print ('--------------------')
+  print('value:\t\t{}'.format(carried_kg))
+ks_larry()
     
