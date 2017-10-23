9efd29cbda61664e4ea4ca7fb343bb1c096b2b3a
Tony Hajdini <MyNameIsBond@users.noreply.github.com>
2017-10-23 19:40:09 +0100

    Create week_2.py

---
+++ week_2.py
@@ -0,0 +1,29 @@
+
+l1 = [3,1,9]
+l2 = [2,1,2,1]
+
+def polynomial(l1,l2):
+	solve = []
+	counter = -1
+	for i in l1:
+		solve.append(i)
+polynomial(l1,l2)
+
+
+
+
+
+
+
+class Polynomial:
+	solve = []
+	
+	def __init__(self,l1,l2):
+		self.l2 = l2
+		self.l1 = l1
+		
+	def __str__(self):
+		for i in self.solve:
+			return i
+	
+	
