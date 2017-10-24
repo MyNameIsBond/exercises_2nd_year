a475c163432fb5ce602317bbacecb51c4eb625cb
Tony Hajdini <hajdinim@uni.coventry.ac.uk>
2017-10-24 03:35:38 +0100

    Adding DONE!

--- week_2.py
+++ week_2.py
@@ -1,24 +1,30 @@
 
-l1 = [3,1,9,2,1]
+l1 = [3,1,9]
 l2 = [2,1,2,1]
 
 def polynomial(l1,l2):
 	'''Adds two polynomials which are in a list form. '''
 	solve = []
+	l1 = list(reversed(l1)) 
+	l2 = list(reversed(l2)) 
+	
 	if len(l1) > len(l2):
-		l2 , l1 = l1 , l2  # swap if it is bigger. 
+		l2 , l1 = l1 , l2  # swap if it is bigger.
+
 	for i in l2:
 		solve.append(i)
-	for k in range(len(l1)):
-		solve[k] = solve[k] + l1[k] # adds the two numbers on the same list (solve)	
-	print (solve)
-		
 
+	for k in range(len(l1)):
+		solve[k] = solve[k] + l1[k] # adds the two numbers on the same list (solve)
+		
+	solve = list(reversed(solve))
+	print (solve)
 
 polynomial(l1,l2)
 
 
-class Polynomial:
+
+class Polynomial():
 	solve = []
 	
 	def __init__(self,l1,l2):
@@ -32,16 +38,28 @@ class Polynomial:
 	def __add__(self,l1,l2):
 
 		'''Adds two polynomials which are in a list form. '''
+		self.l1 = list(reversed(self.l1))
+		self.l2 = list(reversed(self.l2))
+
 		self.solve = []
+
 		if len(self.l1) > len(self.l2):
 			self.l2 , self.l1 = self.l1 , self.l2  # swap if it is bigger.
+
 		for i in self.l2:
 			self.solve.append(i)
+
 		for k in range(len(self.l1)):
 			self.solve[k] = self.solve[k] + self.l1[k] # adds the two numbers on the same list (solve)
 		return self.solve
+
 	def __mul__(self,l1,l2):
 		pass
 
 
+
+
+
+
 a = Polynomial(l1,l2)
+# print (a.__add__(l1 , l2))
