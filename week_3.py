4c97754e42c2cc9afe9027e846a5139e66eb09bd
Tony Hajdini <hajdinim@uni.coventry.ac.uk>
2017-10-26 00:38:28 +0100

    solve it!

--- week_3.py
+++ week_3.py
@@ -6,22 +6,19 @@
 
 def prints_backwards(number,string):
 	''' Prints a string seperated by spaces.'''
-	if number <= 0:
+	
+	if number <= 0: # this is the way out. 
 		return None
+
 	else:
 		if type(string) == str:
 			string = string.split(' ')
-			print (number)
-			for i in range(len(string)):
-				s = string[i]
-				s = s[::-1]
-				print (s)
-		# string = reversed(string)
-		print ('\t:{}'.format(string[number]))
-		prints_backwards(number-1,string)
+			if number >= len(string):
+				number = len(string) # make sure the function will run. 
 
-
+		print ('\t:{}'.format(string[number][::-1]))
+		return prints_backwards(number-1,string)
 
 string = input ('Give a string to be mirrored\t')
-
-prints_backwards(4,string)
+number = int (input('How many times you want the recursion to run? :)\t'))
+prints_backwards(number,string)
