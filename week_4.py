151ca296e4d8f158242f988e0d83f8a27ffd9a19
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2017-11-01 19:58:18 +0000

    ex 1

--- week_4.py
+++ week_4.py
@@ -1,19 +1,25 @@
-class all_sorts:
+
+class All_sorts:
 
     compare_number  = None
     loop_counter    = 0
 
     def __init__ (self,number_list):
         self.number_list = number_list
-
+        
     def insertion(self,number_list):
 
-        for number in self.number_list:
-            if compare_number == None:
-                number = compare_number
-            elif compare_number > number:
+        for i in range(len(self.number_list)):
+            print (self.number_list[i])
+            if self.compare_number == None:
+                self.compare_number = i
+                print ('oops!')
 
-            loop_counter += 1
 
-                
+
+    # def bouble_sort(self,number_list):
+l = [1,2,4,5,6,7,8] 
+
+a = All_sorts(l)
+a.insertion(l)
 
