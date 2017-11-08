0edce500177e59c5081f422726e39f556bdc3b10
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2017-11-08 00:31:52 +0000

    more stuff

---
+++ week_4ad1.py
@@ -0,0 +1,24 @@
+'''
+1. Consider a n x m matrix, with elements decimal digits (natural numbers between 1 and 9), representing
+colours. A connected set associated to an element is the set of elements that may be reached from this
+element, by successive moves on a same row or column preserving the same color. It is to determine
+the size and the colour of the biggest connected set. In case of multiple solutions, display them all.
+Input: n = 5 m = 6 and the values within the matrix are randomised, between 1 and 9)
+1 3 5 6 6 6
+8 8 5 2 2 2
+2 8 8 3 4 1
+7 1 8 5 9 9
+9 8 3 4 2 2
+Output: The size of the biggest set is 5 and the colour is whichever colour you represented by 8.
+'''
+
+class Matrix:
+
+    ''' Create a matrix according to inputs '''
+
+    def __init__(self,n,m):
+        self.n = n
+        self.m = m
+
+
+    def make_matrix(self):
