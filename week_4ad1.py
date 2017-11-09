288247c91fa6690dd914e119be6924ad1ab57216
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2017-11-09 15:09:51 +0000

    Adv 1 week 4.

--- week_4ad1.py
+++ week_4ad1.py
@@ -4,21 +4,34 @@ colours. A connected set associated to an element is the set of elements that ma
 element, by successive moves on a same row or column preserving the same color. It is to determine
 the size and the colour of the biggest connected set. In case of multiple solutions, display them all.
 Input: n = 5 m = 6 and the values within the matrix are randomised, between 1 and 9)
-1 3 5 6 6 6
-8 8 5 2 2 2
-2 8 8 3 4 1
-7 1 8 5 9 9
-9 8 3 4 2 2
+
+                                        1 3 5 6 6 6
+                                        8 8 5 2 2 2
+                                        2 8 8 3 4 1
+                                        7 1 8 5 9 9
+                                        9 8 3 4 2 2
+
 Output: The size of the biggest set is 5 and the colour is whichever colour you represented by 8.
 '''
-
-class Matrix:
-
+import random as rnd
+class MMatrix:
     ''' Create a matrix according to inputs '''
+
 
     def __init__(self,n,m):
         self.n = n
         self.m = m
 
+    def make_matrix(self,n,m):
+        colour = ['red','yellow','green','pink','grey','orange','blue','violet','aqua','black','white']
+        mat = [[rnd.randint(1,9) for i in range(n)] for i in range(m)] # creates a matrix! n x m
+        print('\n'.join([''.join(['{:2}'.format(number) for number in row]) for row in mat]))
 
-    def make_matrix(self):
+if __name__ == '__main__':
+    try:
+        n = int(input('type the length(rows)\t'))
+        m = int(input('type the width(column)\t'))
+        a = MMatrix(n,m)
+        a.make_matrix(n,m)
+    except TypeError:
+        print ('type a number.')
