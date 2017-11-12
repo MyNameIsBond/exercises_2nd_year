3531efdb696328199c860aac9c6585988d1f3bf7
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2017-11-12 05:58:25 +0000

    week 4 ad 1. Done!

--- week_4ad1.py
+++ week_4ad1.py
@@ -27,21 +27,18 @@ class Matrix:
         and which colour is the set of numbers '''
 
     def __init__(self,n,m):
+
         self.n = n
         self.m = m
         self.c = Counter({})
-        self.colour = ['red','yellow','green','pink','grey','orange','blue','violet','aqua','black','white']
+        self.colour = ['red','yellow','green','pink','grey','orange','blue','black','white']
 
-kdsajdlsjdlk jasd
-dasd sadasdsad
-
-    def make_matrix(self,n,m):
+    def create_matrix(self,n,m):
         ''' creates the matrix according to the inputs. '''
 
         self.mat = [[rnd.randint(1,9) for i in range(n)] for i in range(m)] # creates a matrix ! n x m
         print('\n'.join([' '.join(['{}'.format(number) for number in row]) for row in self.mat]))
         return self.mat
-
 
     def check_set(self):
         '''set will be checked '''
@@ -52,40 +49,40 @@ dasd sadasdsad
             for i in range(len(matrx[row])-1):
 
                 if matrx[row][i] == matrx[row][i+1]: # checking the next intance
-                    self.c.update({self.colour[matrx[row][i]]})
+                    self.c.update({self.colour[matrx[row][i]-1]})#-1,to cover 0 instance in colour list. 
 
                 if matrx[row][i] == matrx[row+1][i]: # checking the row's  next intance
-                    self.c.update({self.colour[matrx[row][i]]})
-
-                if i == len(matrx[row]) - 2: # checking the last's row instances
-
-                    if matrx[row][i+1] == matrx[row+1][i+1]:
-                        self.c.update({self.colour[matrx[row][i+1]]})
+                    self.c.update({self.colour[matrx[row][i]-1]})
 
                 if row == len(matrx) - 2: # checking the corners of the matrix
 
                     if matrx[row+1][i] == matrx[row+1][i+1]:
-                        self.c.update({self.colour[matrx[row+1][i]]})
+                        self.c.update({self.colour[matrx[row+1][i]-1]})
+
+                if i == len(matrx[row]) - 2:
 
                     if matrx[row][i+1] == matrx[row+1][i+1]:
-                        self.c.update({self.colour[matrx[row+1][i+1]]})
+                        self.c.update({self.colour[matrx[row+1][i+1]-1]})
+
 
         return self.c
 
 
     def print_colour(self):
-        for i,r in self.c.most_common(1):
-            print ('{}{}'.format(i,r)) # Times two, so we count both elements.
-        print ('hey: {}'.format(self.c))
+
+        for values,keys in self.c.most_common(1):
+            print ('the colour is {}. while the biggest set was {}.'.format(values,keys+1))
 
 
 if __name__ == '__main__':
+
     try:
         n = int(input('type the column\t'))
         m = int(input('type the rows\t'))
         a = Matrix(n,m)
-        a.make_matrix(n,m)
+        a.create_matrix(n,m)
         a.check_set()
         a.print_colour()
+
     except (TypeError,ValueError):
         print ('type a number.')
