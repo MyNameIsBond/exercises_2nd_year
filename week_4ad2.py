5d671b17044ced76af7c55790e37279bd07fbd5d
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2017-11-13 07:58:58 +0000

    w4 DONE!

--- week_4ad2.py
+++ week_4ad2.py
@@ -1,24 +1,74 @@
 
-'''2. Adapt the Quick Sort algorithm to find the mth smallest element out of a list of n integers, where m is
+'''
+2. Adapt the Quick Sort algorithm to find the mth smallest element out of a list of n integers, where m is
 read from the standard input.
 For example, when m = 5, your version of Quick Sort will output the 5th smallest element out of your
-input list.'''
+input list.
+'''
 
 from random import randint as rint
 class Smallest:
-    ''' The smallest (number)th element will be printed out. using quick sort'''
+    ''' The smallest (number)th element will be printed out. using quicksort'''
+
     def __init__(self,number):
-        self.number      = number
-        self.random_list = [rint(1,20) for x in range(rint(1,20))] # create a random list.
+
+        self.number = number
+        self.r_list = [rint(1,20) for x in range(rint(10,20))] # create a random list.
+
+        lower = 0
+        high  = len(self.r_list) - 1
+        self.quicksort(lower,high)
 
 
-    def quick_sort(self):
-        '''the quick sort'''
+    def quicksort(self,lower,high):
+        '''the quicksort.'''
 
-        for i in self.random_list:
-            print (i)
+        if lower < high:
+            p = self.partition (lower,high)
+            self.quicksort(lower,p - 1)
+            self.quicksort(p+1,high)
+
+    def partition(self,lower,high):
+        ''' break the list. '''
+
+        pivot = self.r_list[high]
+        wall = lower - 1
+        for j in range(lower,high):
+
+            if self.r_list[j] < pivot:
+                wall = wall + 1
+                self.r_list[wall],self.r_list[j] = self.r_list[j],self.r_list[wall]
+
+        if self.r_list[high] < self.r_list[wall + 1]:
+            self.r_list[wall + 1],self.r_list[high] = self.r_list[high],self.r_list[wall + 1]
+
+        return wall + 1
+
+    def check_number(self):
+        '''print out the index of the given number. '''
+
+        if self.number in self.r_list:
+            if self.r_list.index(self.number)+1 == 1:
+                print ('{}st element'.format(self.r_list.index(self.number)+1))
+            elif self.r_list.index(self.number)+1 == 2:
+                print ('{}nd element'.format(self.r_list.index(self.number)+1))
+            elif self.r_list.index(self.number)+1 == 3:
+                print ('{}rd element'.format(self.r_list.index(self.number)+1))
+            elif self.r_list.index(self.number)+1 > 3:
+                print ('{}th element'.format(self.r_list.index(self.number)+1))
+        else:
+            print ('Not in my list.')
+
+    def __str__(self):
+        '''prints out the list. '''
+        return str(self.r_list)
 
 
 if __name__ == '__main__':
-    a = Smallest(5)
-    a.quick_sort()
+    try:
+        a = Smallest(5)
+        a.check_number()
+        print (a)
+
+    except (TypeError,ValueError):
+        print ('the Input should be a number.')
