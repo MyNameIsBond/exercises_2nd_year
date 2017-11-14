e0855cb16dd00fc4d64e01e663c0e5c8c068544e
Tony Hajdini <MyNameIsBond@users.noreply.github.com>
2017-11-14 15:40:56 +0000

    Update ex1.py

--- week_5/ex1.py
+++ week_5/ex1.py
@@ -1,4 +1,4 @@
-class Node(object):
+class Node:
     ''' Create Linked list'''
     def __init__(self,data):
         self.data = data
@@ -13,27 +13,36 @@ class Node(object):
         ''' get the next element '''
         return self.next
 
-    def set_next_node(self,data):
+    def set_next_node(self,newnext):
         ''' set the element'''
+
+        self.next = newnext
+
+    def set_data(self,newdata):
 
         self.data = newdata
 
 
 class Linked_list:
-    '''can create a linked list. '''
+    ''' can create a linked list. '''
 
     def __init__(self,head=None):
         self.head = head
 
 
     def append(self,data):
+
         new_node = Node(data)
         new_node.set_next_node(self.head)
         self.head = new_node
+
+    def empty(self):
+
+        return self.head == None
         
 
     def delete(self,data):
-        '''Deletes an instance in the linked list'''
+        ''' Deletes an instance in the linked list '''
 
         current_node  = self.head
         previous_node = None
@@ -47,7 +56,10 @@ class Linked_list:
             self.head = current.get_next()
         else:
             previous_node.set_next_node(current.get_next())
+
     def len(self):
+        ''' return the length of the list. (int) '''
+
         current_node = self.node
         counter = 0
         while current_node != None:
@@ -56,23 +68,24 @@ class Linked_list:
 
             
     def __str__(self):
-        result =  []
+        ''' prints out the list. '''
+        result =  ','
         node = self.head
-        while node != None:
-            node = node.get_next()
-            result.append(node)
+        if node != None:
+            result +=','+ str(node)
+            
+            while node:
+                result +=','+ str(node)
+                node = node.get_next()
+
         return result
 
 
 
 if __name__ == '__main__':
-    a = Linked_list.append(2)
-    # a = Linked_list.set_data(4)
-    # a = Linked_list.set_data(5)
-    # a = Linked_list.set_data(6)
-    print (a)
-
-
-
-
- 
+    my_list = Linked_list()
+    my_list.append(4)
+    my_list.append(5)
+    my_list.append(6)
+    my_list.append(7)
+    print (my_list)
