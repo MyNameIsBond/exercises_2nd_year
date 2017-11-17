d47331a3290da76b6e7a908e0fb341394500a3f6
Tony Hajdini <MyNameIsBond@users.noreply.github.com>
2017-11-17 20:32:41 +0000

    Update ad1.py

--- week_5/ad1.py
+++ week_5/ad1.py
@@ -1,7 +1,4 @@
 import random as rnd
-
-
-
 class Sum_check:
 
 	def __init__(self,n,m):
@@ -15,39 +12,47 @@ class Sum_check:
 	def create_matrix(self):
 		''' creates the matrix according to the inputs. '''
 
-		self.mat = [[rnd.randint(1,9) for i in range(self.m)] for i in range(self.m)] #matrix: n x m
+		self.mat = [[rnd.randint(1,9) for i in range(self.n)] for i in range(self.n)] #matrix: n x n
 
 		return self.mat
 
 	def check_matrix(self):
 		''' creating a list of diagonal inputs '''
+
 		matrix = range(len(self.mat))
 
 		for i in matrix:
 			self.checking_list.append(self.mat[i][i])
 
 	def check_sum(self):
-		''' sort the list and sum the up 'n' numbers '''
+		''' sorts the list and sums up 'n' numbers '''
 
-		sorted_list = sorted(self.checking_list, reverse= True)
-		if m > len(sorted_list):
-			for i in range(m):
+		sum_up = 0
+		sorted_list = sorted(self.checking_list,reverse=True)
+
+		if self.m <= len(sorted_list):
+			for i in range(self.m):
+				print (sorted_list[i])
+				sum_up += sorted_list[i]
+			print (''.join(sorted_list[i]))
+
 		else:
-
-			print ('insert a smaller number.')
-
-
-
-
+			print ('type a smaller number.')
+			print ('the list is not that big.')
+		print (sum_up)
 
 	def __str__(self):
+		''' print the matrix '''
 
 		return ('\n'.join([' '.join(['{}'.format(number) for number in row]) for row in self.mat]))
 
-
 if __name__ == '__main__':
-	a = Sum_check(5,5)
-	a.create_matrix()
-	a.check_matrix()
-	a.check_sum()
-	print (a)
+	try:
+		m = int(input('How many numbers you want to sum up?\t'))
+		a = Sum_check(5,m)
+		a.create_matrix()
+		a.check_matrix()
+		a.check_sum()
+		print (a)
+	except ValueError:
+		print ('a number Should be given')
