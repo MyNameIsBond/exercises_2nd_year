89c03e5e4dd76d6555732fa412f401f3b8ed450e
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2017-12-04 04:11:26 +0000

    the dream is here

---
+++ SQL_MongoDB/bd_male_female.py
@@ -0,0 +1,65 @@
+import numpy
+
+
+class Male_female_school(object):
+	"""returns"""
+	def __init__(self, male,female):
+		self.male = male
+		self.female = female
+
+	def grades(self):
+		pass
+
+	def graduates(Self):
+		pass
+
+class Male_female_court(object):
+
+	def __init__(self,male,female):
+
+		self.male = male
+		self.female = female
+
+	def prison_perc(self):
+		pass
+
+
+	def marriage_cases(self):
+		pass
+
+	def filling_devorse(self):
+		pass
+
+	def alimoni_win(self):
+		pass
+
+	def children_win(self):
+		pass
+
+class Male_female_suicide(object):
+
+
+	def __init_(self,male,female):
+		self.male = male
+		self.female = female
+
+	def after_court(self):
+		pass
+
+	def after_marriage(self):
+		pass
+
+	def after_30s(self):
+		pass
+
+
+
+class Male_female_employability(object):
+
+	def __init__(self,male,female):
+		self.male = male
+		self.female = female
+
+	def wage(self):
+		pass
+		
